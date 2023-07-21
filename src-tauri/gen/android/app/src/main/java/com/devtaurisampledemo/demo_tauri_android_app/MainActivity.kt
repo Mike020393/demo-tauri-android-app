81f218d80023aaf6787b8c9e28c9bf17e67b9d84
Mike020393 <mike.w020393@gmail.com>
2023-07-21 20:07:30 +0530

    Initial Commit

---
+++ src-tauri/gen/android/app/src/main/java/com/devtaurisampledemo/demo_tauri_android_app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.devtaurisampledemo.demo_tauri_android_app
+
+class MainActivity : TauriActivity()
---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Greet from './lib/Greet.svelte'
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri! Demo Applications </h1>
+
+  <h2> Developer Title in Svelte </h2>
+
+  <h3> Testing Title For Update Test </h3>
+
+  <div class="row">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank">
+      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
+    </a>
+    <a href="https://tauri.app" target="_blank">
+      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    </a>
+  </div>
+
+  <p>
+    Click on the Tauri, Vite, and Svelte logos to learn more.
+  </p>
+
+  <div class="row">
+    <Greet />
+  </div>
+
+</main>
+
+<style>
+  .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+</style>
